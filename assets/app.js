c98e885d2ef68039c084ad00e083da538354eeb8
viktoriia <victoria.kapyrina@hosco.com>
2025-04-09 16:34:12 +0300

    base twig skeleton

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,7 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+// Import styles at the end, so default plugins styles can be overridden when neededы
+import './styles/app.scss';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
